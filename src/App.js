a10d8f842ae4c7c2c1619fa9d5135eb5680f22f3
Sergey <rodenkosv@gmail.com>
2024-04-11 16:22:55 +0700

    Homework 4, adding files and code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './component/HomePage';
+import AboutPage from './component/AboutPage';
 
 function App() {
   return (
     <div className="App">
       <h2>Урок 8. Семинар: Children. Роутинг в React</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/component/AboutPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function AboutPage() {
+    return (
+        <div>
+            <h1>О нас</h1>
+            <Link to="/">Главная</Link>
+        </div>
+
+    );
+}
+
+export default AboutPage;
